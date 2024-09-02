73725c5f998222149391721261ce556de4447125
harramzahid <harramzahid5@gmail.com>
2024-09-01 21:25:41 -0400

    Task 2 Compound Data

--- cc_1.js
+++ cc_1.js
@@ -6,4 +6,14 @@ var isActive = true;
 console.log(employeeName, typeof(employeeName));
 console.log(employeeID, typeof(employeeID));
 console.log(isActive, typeof(isActive));
- 
+
+//Task 2 Compound Data Types
+
+let products = ("lip gloss", "foundation", "eyeshadow");
+const productDetails = { 
+    name: self-care,
+    price: 233.00,
+    inStock: yes , 
+}
+
+console.log(products, productDetails);
