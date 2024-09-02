c03ece597a5937c1325ecf06addc559f83d6c1cf
harramzahid <harramzahid5@gmail.com>
2024-09-01 21:07:48 -0400

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,9 @@
+//Task 1 Variables and Data Types
+
+let employeeName = "Fatima Taj";
+const employeeID = 4;
+var isActive = true;
+console.log(employeeName, typeof(employeeName));
+console.log(employeeID, typeof(employeeID));
+console.log(isActive, typeof(isActive));
+ 
