76373a5ff8bc4e1fb4dd7be2ce131b9a62bb65af
harramzahid <harramzahid5@gmail.com>
2024-09-01 23:29:14 -0400

    Task 4 Comparison Operators

--- cc_1.js
+++ cc_1.js
@@ -40,3 +40,29 @@ accountBalance%= 7
 accountBalance = 5
 console.log("After %= 7:", accountBalance)//
 console.log(accountBalance)
+
+//Task 4 Comparison Operators
+let empployeescore1 = 5
+let empployeescore2 = 6
+
+let comparison1 = empployeescore1 > empployeescore2;
+console.log("comparison of empployeescore1 > empployeescore2", comparison1);
+
+let comparison2 = empployeescore1 < empployeescore2;
+console.log("comparison of empployeescore1 < empployeescore2", comparison2);
+
+let comparison3 = empployeescore1 >= empployeescore2;
+console.log("comparison of empployeescore1 >= empployeescore2", comparison3);
+
+let comparison4 = empployeescore1 <= empployeescore2;
+console.log("comparison of empployeescore1 <= empployeescore2", comparison4;
+
+let comparison5 = empployeescore1 === empployeescore2;
+console.log("comparison of empployeescore1 === empployeescore2", comparison5);
+
+let comparison6 = empployeescore1 !== empployeescore2;
+console.log("comparison of empployeescore1 !== empployeescore2", comparison6);
+
+
+
+
